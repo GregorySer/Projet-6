67f80aadcefa2e687f05a5d0a8d45de88af8e064
Gregory Sergent <sergent.gregory@gmail.com>
2021-09-28 01:28:25 +0200

    AD_python_v1.1.py

--- AD_python_v1.1.py
+++ AD_python_v1.1.py
@@ -1,6 +1,7 @@
 #importer les packages nécessaires
 import csv
 import shutil
+import numpy as np
 
 #Lit le fichier csv de base
 with open('fichier_brut.csv') as fichier_csv:
@@ -10,8 +11,8 @@ with open('fichier_brut.csv') as fichier_csv:
 
 
 #copier fichier_brut vers fichier_temp1
-source=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet 6\fichier_brut.csv'
-destination=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet 6\fichier_temp1.csv'
+source=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet-6\fichier_brut.csv'
+destination=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet-6\fichier_temp1.csv'
 shutil.copyfile(source, destination)
 
 
@@ -23,7 +24,8 @@ Nom = []
 
 # Lit le fichier temp1
 with open('fichier_temp1.csv') as fichier_csv:
-    reader = csv.reader(fichier_csv, delimiter=',')
+    #ATTENTION AU CHOIX DU DELIMITEUR!!!!
+    reader = csv.reader(fichier_csv, delimiter=';')
         #row --> met le tableur sous forme de listes de listes ; Tableau =[[prenom1, nom1],[prenom2,nom2]...]
     for row in reader:
             Tableau.append(row)
@@ -32,22 +34,27 @@ with open('fichier_temp1.csv') as fichier_csv:
 #contrôler qu'on est passé en mode liste de liste
 print(Tableau)
 
-#creer colonne "Identifiant" et fonction "f_identifiant"
-titre='Identifiant'
-f_identifiant=row[1]+row[0:3]+1
+#nommer une colonne
+ajout_colonne='identifiant'
+i=0
 
-#creer fonction "identifiant"
-ajout_colonne_identifiant('fichier_temp1.csv', 'fichier_temp_colonn.csv',
-                          lambda row, line_num: row.append(titre) if line_num==1 else
-                          row.append(f_identifiant))
+#Modifier la ligne en la passant à la fonction transform_row
+for row in Tableau:
+    row.append(ajout_colonne)
 
-print(fichier_temp_colonn.csv)
+print(Tableau)
+RechDoublon={}
+for i in range(len(Tableau)):
+    
+    if i==0:
+        Tableau[i]=[Tableau[i][0],Tableau[i][1], 'Identifiants']
+    else:
+        Tableau[i]=[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1']
+        if 'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1' in RechDoublon:
+          Tableau[i]=[Tableau[i][0],Tableau[i][1], 'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'2']
+          print('Attention')
+        else:
+            print('OK')
+        RechDoublon['u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1']=[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1']
 
-#on récupère les données:
-#for i in range(Prenom.append(Tableau[i][0])
-#        Nom.append(Tableau[i][1])
-#    print(Prenom, Nom)
-
-
-
-
+print(Tableau)
