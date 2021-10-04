c31624a20777dc2801b1ac75b50711ba82ab9cfe
Gregory Sergent <sergent.gregory@gmail.com>
2021-10-04 23:36:11 +0200

    AD_python_v1.1.py

--- AD_python_v1.1.py
+++ AD_python_v1.1.py
@@ -4,6 +4,7 @@ import shutil
 import numpy as np
 from pyad import *
 
+
 #Lit le fichier csv de base
 with open('fichier_brut.csv') as fichier_csv:
     reader = csv.reader(fichier_csv, delimiter=',')
@@ -12,13 +13,9 @@ with open('fichier_brut.csv') as fichier_csv:
 
 
 #copier fichier_brut vers fichier_temp1
-<<<<<<< HEAD
 source=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet-6\fichier_brut.csv'
 destination=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet-6\fichier_temp1.csv'
-=======
-source=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet 6\fichier_brut.csv'
-destination=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet 6\fichier_temp1.csv'
->>>>>>> 02396dbcea835e44912d31b19e0a7162bb0c12e8
+
 shutil.copyfile(source, destination)
 
 
@@ -30,12 +27,8 @@ Nom = []
 
 # Lit le fichier temp1
 with open('fichier_temp1.csv') as fichier_csv:
-<<<<<<< HEAD
     #ATTENTION AU CHOIX DU DELIMITEUR!!!!
     reader = csv.reader(fichier_csv, delimiter=';')
-=======
-    reader = csv.reader(fichier_csv, delimiter=',')
->>>>>>> 02396dbcea835e44912d31b19e0a7162bb0c12e8
         #row --> met le tableur sous forme de listes de listes ; Tableau =[[prenom1, nom1],[prenom2,nom2]...]
     for row in reader:
             Tableau.append(row)
@@ -44,7 +37,6 @@ with open('fichier_temp1.csv') as fichier_csv:
 #contrôler qu'on est passé en mode liste de liste
 print(Tableau)
 
-<<<<<<< HEAD
 #nommer une colonne
 ajout_colonne='identifiant'
 i=0
@@ -53,7 +45,7 @@ i=0
 for row in Tableau:
     row.append(ajout_colonne)
 
-print(Tableau)
+
 RechDoublon={}
 for i in range(len(Tableau)):
     
@@ -68,14 +60,23 @@ for i in range(len(Tableau)):
             print('OK')
         RechDoublon['u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1']=[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1']
 
-print(Tableau)
-=======
-#on récupère les données:
-#for i in range(Prenom.append(Tableau[i][0])
-#        Nom.append(Tableau[i][1])
-#    print(Prenom, Nom)
+
+#Ajouter des colonnes au tableau A FAIRE PLUS TARD
+        
+
+#préparer le module pyad: définir le compte admin pour faire les manip auto
+pyad.set_defaults(ldap_server="ADPython.com", username="Administrateur", password="Secret12")
+ou=pyad.adcontainer.ADContainer.from_dn("ou=Groupes, dc=ADPython, dc=com")
 
 
-#Ajouter des colonnes au tableau
-
->>>>>>> 02396dbcea835e44912d31b19e0a7162bb0c12e8
+#ajouter les utilisateurs en lisant le csv:
+new_group=pyad.adgroup.ADGroup.from_cn('employee')
+new_user=[]
+for row in Tableau:
+    givenName=row[0]
+    name=row[1]
+    sAMAccountName=row[2]
+    print(givenName, name, sAMAccountName)
+    new_user.append(pyad.aduser.ADUser.create(sAMAccountName, ou, 'Azerty1234+'))
+    
+new_group.add_members(new_user)
