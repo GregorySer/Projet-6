3bda8e2c85b076f6f69c60eb91e7602966fbb701
Gregory Sergent <sergent.gregory@gmail.com>
2021-10-18 17:28:51 +0200

    Pour livrable projet 6

--- AD_python_v1.1.py
+++ AD_python_v1.1.py
@@ -1,3 +1,5 @@
+#VERSION V1.7
+
 #importer les packages nécessaires
 import csv
 import shutil
@@ -13,8 +15,8 @@ with open('fichier_brut.csv', encoding='utf-8') as fichier_csv:
 
 
 #copier fichier_brut vers fichier_temp1
-source=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet-6\fichier_brut.csv'
-destination=r'C:\Users\Greg\Documents\Formations\INFO\OCR\Admin infra et cloud\PROJETS\Projet-6\fichier_temp1.csv'
+source=r'C:\Users\mon\Fichier\brut.csv'
+destination=r'C:\Users\mon\Fichier\fichier_temp1.csv'
 
 shutil.copyfile(source, destination)
 
@@ -35,15 +37,16 @@ with open('fichier_temp1.csv', encoding='utf-8') as fichier_csv:
     fichier_csv.close
 
 #contrôler qu'on est passé en mode liste de liste
-print(Tableau)
+#print(Tableau)
 
-#nommer une colonne
+#créations de variables: nommer une colonne et initialiser compteur
 ajout_colonne='identifiant'
 i=0
 
-#Modifier la ligne en la passant à la fonction transform_row
+#Ajouter colonne au tableau
 for row in Tableau:
     row.append(ajout_colonne)
+
 
 #recherche de doublon dans les identifiants
 RechDoublon={}
@@ -61,8 +64,6 @@ for i in range(len(Tableau)):
         RechDoublon['u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1']=[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1']
 
 #2eme recherche de doublons
-print('2eme recherche de doublons')
-print(Tableau)
 RechDoublon2={}
 for i in range(len(Tableau)):
     if Tableau[i]==[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'2'] and Tableau[i-1]==[Tableau[i-1][0],Tableau[i-1][1],'u'+Tableau[i-1][0][0]+Tableau[i-1][1][0:3]+'2']:
@@ -70,27 +71,37 @@ for i in range(len(Tableau)):
         print('Attention')
     else:
         print('OK')
-#        RechDoublon2['u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'2']=[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'2']
-    
 
 
-#Ajouter des colonnes au tableau A FAIRE PLUS TARD
-        
+#Supprimer la première ligne qui correspond aux titres des colonnes
+del Tableau[0]
+
 
 #préparer le module pyad: définir le compte admin pour faire les manip auto
-pyad.set_defaults(ldap_server="ADPython.com", username="Administrateur", password="Secret12")
+pyad.set_defaults(ldap_server="ADPython.com", username="Administrateur", password="xxxxxxxxxx")
 ou=pyad.adcontainer.ADContainer.from_dn("ou=Groupes, dc=ADPython, dc=com")
 
-#pour y voir plus clair :-)
-NewTableau=Tableau
+#####CONTROLER DOUBLON DE L'AD :evolution a etudier
+#n=1
+#for i in range(len(Tableau)):
+#       Tableau[i]=[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]]
+#       while 'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+str(n) in ou:
+#           n=n+1
+#           Tableau[i]=[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+str(n)]
+#       Tableau[i]=[Tableau[i][0],Tableau[i][1],'u'+Tableau[i][0][0]+Tableau[i][1][0:3]+'1']
 
 
 #ajouter les utilisateurs en lisant le csv:
 new_group=pyad.adgroup.ADGroup.from_cn('employee')
 new_user=[]
-for row in NewTableau:
+for row in Tableau:
     givenName=row[0]
     name=row[1]
     sAMAccountName=row[2]
     print(givenName, name, sAMAccountName)
-    new_user.append(pyad.aduser.ADUser.create(sAMAccountName, ou, 'Azerty1234+', optional_attribu
+    new_user.append(pyad.aduser.ADUser.create(sAMAccountName, ou, 'xxxxxxxxxxx', optional_attributes={"givenName":givenName, "displayName":name+' '+givenName, "sn":name}))
+    
+new_group.add_members(new_user)
+
+print('Les comptes utilisateurs ont été créés')
+
